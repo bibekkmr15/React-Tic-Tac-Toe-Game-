a6ee7f0e77daf8dd8c32f054c767d308a0648d8c
Bibek Kumar Buranwal <bibekkmr15@gmail.com>
2023-12-04 12:41:29 +0530

    edit main 'index.html' page and add 2 players in 'App.jsx' file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,30 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to the Game - React Tic-Tac-Toe</h1>
-    </>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+              <button>Edit</button>
+            </span>
+          </li>
+        </ol>
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+              <button>Edit</button>
+            </span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      Log
+    </main>
   );
 }
 
